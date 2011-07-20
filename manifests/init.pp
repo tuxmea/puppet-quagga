a27e0472933ee6520538e81e37dcc8cbb0f4928a
Martin Alfke <martin.alfke@buero20.org>
2011-07-20 16:32:02 +0200

    add params, use params in init, add package and service to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,28 @@
-# module quagga - deploy quagga bgp service
+# Class: quagga 
+#
+# This class installs quagga daemon
+#
+# Parameters:
+#
+# Actions:
+# - Install quagga
+# - Manage quagga services
+#
+# Requires:
+#
+# Sample usage:
+# - include quagga
 #
 class quagga {
-
+	include quagga::params
+	package { 'quagga':
+		name		=> $quagga::params::quagga_name,
+		ensure		=> installed,
+	}
+	service { 'quagga':
+		name		=> $quagga::params::quagga_name,
+		ensure		=> running,
+		enable		=> true,
+		subscribe	=> Package['quagga'],
+	}
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,31 @@
-# sub module quagga::params - define parameters
+# Class: quagga::params
+#
+# This class manages Quagga parameters
+#
+# Parameters:
+# - The $user that Quagga runs as
+# - The $group that Quagga runs as
+# - The $quagga_name is the name of the package and service on the relevant distribution
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
 #
 class quagga::params {
 
+	$user		= quagga
+	$group		= quagga
+	$quagga_name	= quagga
+
+	case $operatingsystem {
+		'ubuntu','debian': {
+			$quagga_conf		= '/etc/quagga'
+			$quagga_daemons_conf	= '/etc/quagga/daemons'
+			$quagga_debian_conf	= '/etc/quagga/debian.conf'
+			$quagga_zebra_conf	= '/etc/quagga/zebra.conf'
+			$quagga_bgp_conf	= '/etc/quagga/bgpd.conf'
+		}
+	}
 }
